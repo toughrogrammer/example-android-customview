f26f90e8cafe0e34ad645890164a90ed2a7893a1
growingdever <growingdever@gmail.com>
2013-11-03 02:15:21 +0900

    first commit

---
+++ src/com/example/customviewproject/MyFolder.java
@@ -0,0 +1,26 @@
+package com.example.customviewproject;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MyFolder extends View{
+
+	public MyFolder(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public MyFolder(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public MyFolder(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
